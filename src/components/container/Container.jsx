32bce07db8c92ce145f4eaf76e2184ec8b523ef3
saksham1782006 <sakmishra433@gmail.com>
2025-11-20 14:15:34 +0530

    Refactor Container component structure and styles
    Refactor Container component for improved readability and styling.

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React from "react";
 
-
-
-function Container({children, className = ''}) {
+function Container({ children, className = "" }) {
   return (
-    <div className={`w-full max-w-7xl mx-auto px-4 sm:px-6 md:px-8 lg:px-12 transition-all duration-300 ${className}`}>
+    <div
+      className={`
+        w-full max-w-7xl mx-auto px-4 sm:px-6 md:px-8 lg:px-12 
+        transition-all duration-300 
+        
+        bg-[#05070d]/60 
+        border border-cyan-800 
+        shadow-[0_0_25px_#00eaff55] 
+        backdrop-blur-xl 
+        rounded-2xl 
+        
+        hover:shadow-[0_0_40px_#00eaffaa] 
+        hover:border-cyan-400 
+        hover:-translate-y-1
+
+        ${className}
+      `}
+    >
       {children}
     </div>
   );
 }
 
-
-
-export default Container
+export default Container;
